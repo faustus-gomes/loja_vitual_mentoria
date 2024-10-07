0c4f5bcd15322dc6fbdd6abeb395a7ec736c81ed
faustus-gomes <faustus.gomes@gmail.com>
2024-10-07 10:56:48 -0300

    Arquivos iniciais do Projeto - 07/outubro/2024

---
+++ src/test/java/Faustus/loja_virtual_mentoria/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package Faustus.loja_virtual_mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
