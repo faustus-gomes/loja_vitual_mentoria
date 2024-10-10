dc9f4f767378dc2f6d27d75b54c643308a26a424
faustus-gomes <faustus.gomes@gmail.com>
2024-10-09 21:32:31 -0300

    Criado as models->Acesso, CategoriaProduto,Endereco, Pessoa, PF e PJ - 09/outubro/2024

--- src/main/java/Faustus/loja_virtual_mentoria/model/CategoriaProduto.java
+++ src/main/java/Faustus/loja_virtual_mentoria/model/CategoriaProduto.java
@@ -1,19 +1,19 @@
 package Faustus.loja_virtual_mentoria.model;
 
-import java.io.Serializable;
-//import javax.persistence.*;
 import jakarta.persistence.*;
 
-@Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable{
-    private static final long serialVersionUID = 1L;
+import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto",allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
     private Long id;
-    @Column(name = "nomeDesc")
+
+    @Column(name = "nome_desc", nullable = false)
     private String nomeDesc;
 
     public Long getId() {
@@ -39,20 +39,17 @@ public class MarcaProduto implements Serializable{
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
         if (obj == null)
             return false;
-        if (getClass() != obj.getClass())
-            return false;
-        MarcaProduto other = (MarcaProduto) obj;
+        CategoriaProduto other = (CategoriaProduto) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
-        } else if (!id.equals(other.id))
+        }else if (!id.equals(other.id))
             return false;
         return true;
     }
