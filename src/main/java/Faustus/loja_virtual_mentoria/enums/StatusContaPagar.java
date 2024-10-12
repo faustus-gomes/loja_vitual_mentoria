f8df2ff5e215c48e6fc7cc5b3ec4cff8dd4171f9
faustus-gomes <faustus.gomes@gmail.com>
2024-10-12 09:40:50 -0300

    Criado os Enums StatusContasReceber e StatusContasPagar e  models-> Contapagar, ContaReceber e FormaPagamento - 12/outubro/2024

---
+++ src/main/java/Faustus/loja_virtual_mentoria/enums/StatusContaPagar.java
@@ -0,0 +1,24 @@
+package Faustus.loja_virtual_mentoria.enums;
+
+public enum StatusContaPagar {
+    COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada"),
+    NEGOCIADA("Renegociada");
+
+
+    private String descricao;
+
+    private StatusContaPagar(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
+
+}
---
+++ src/main/java/Faustus/loja_virtual_mentoria/enums/StatusContaReceber.java
@@ -0,0 +1,23 @@
+package Faustus.loja_virtual_mentoria.enums;
+
+public enum StatusContaReceber {
+    COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada");
+
+
+    private String descricao;
+
+    private StatusContaReceber(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
+
+}
