6229b5100bc775a054ce20e76242092b4573897c
faustus-gomes <faustus.gomes@gmail.com>
2024-10-12 22:31:29 -0300

    Criado as models-> CupDesc, ImagemProduto, NotaFiscalCompra,NotaItemProduto, produto - 12/outubro/2024

--- src/main/java/Faustus/loja_virtual_mentoria/LojaVirtualMentoriaApplication.java
+++ src/main/java/Faustus/loja_virtual_mentoria/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,10 @@ package Faustus.loja_virtual_mentoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+/*@EntityScan(basePackages = )*/
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
